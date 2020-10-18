ddccf9b0c68e7a88a313fc59f11acc9c2535583b
Flyworkz <pedron.dorian@gmail.com>
2020-10-18 18:17:04 +0200

    connection db OK + test

--- BACK/app/router.js
+++ BACK/app/router.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-
+const db = require('./database');
 const router = Router();
 
-router.get('/test', (req, res) => {
-    res.send('test');
+router.get('/test', async (req, res) => {
+    const testSql = (await db.query(`SELECT * FROM "user";`)).rows;
+    res.json(testSql);
 });
 
 module.exports = router;

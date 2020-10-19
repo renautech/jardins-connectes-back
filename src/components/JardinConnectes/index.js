37c32ad1f1bce7105ca9131c0869ecbc267632de
renautech <renaud.second@outlook.fr>
2020-10-19 10:13:41 +0200

    Component Footer added

--- src/components/JardinConnectes/index.js
+++ src/components/JardinConnectes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from 'src/components/Header';
+import Footer from 'src/components/Footer';
 import Navigation from 'src/components/Navigation';
 
 import './style.scss';
@@ -10,6 +11,7 @@ const JardinConnectes = () => {
     <div className="jardinconnectes">
       <Header />
       <Navigation />
+      <Footer />
     </div>
   );
 };

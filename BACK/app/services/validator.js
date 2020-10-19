6361d4dcbce1802b2446f2f7b194c17424a5c315
Tidus91 <incorvaia.tony@gmail.com>
2020-10-19 10:11:46 +0200

    validator joi OK + user schema (joi)

---
+++ BACK/app/services/validator.js
@@ -0,0 +1,32 @@
+const validateQuery = (schema) => (req,res,next) => {
+    const validatedQuery = schema.validate(req.query);
+
+    if (validatedQuery.error) {
+        res.status(400).json(validatedQuery.error);
+
+    } else {
+        next();
+    }
+};
+
+const validateBody = (schema) => (req,res,next) => {
+    const validatedBody = schema.validate(req.body);
+
+    if (validatedBody.error) {
+        res.status(400).json(validatedBody.error);
+
+    } else {
+        next();
+    }
+};
+
+/*schema.validate({ username: 'abc', birth_year: 1994 });
+// -> { value: { username: 'abc', birth_year: 1994 } }
+
+schema.validate({});
+// -> { value: {}, error: '"username" is required' }*/
+
+module.exports = {
+    validateBody,
+    validateQuery
+};

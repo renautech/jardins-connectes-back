f1807e25aca96517233c6ab5d69c91c2ca281d05
Tidus91 <incorvaia.tony@gmail.com>
2020-10-20 10:08:49 +0200

    server cache REDIS OK

---
+++ BACK/app/cache/cacheStrategy.js
@@ -0,0 +1,52 @@
+const redis = require('redis');
+
+const client = redis.createClient();
+
+const buildKey = (url, params) => {
+    return JSON.stringify({url, params});
+}
+
+const theKeys = [];
+
+const cache = (request,response,next) => {
+    const theKey = buildKey(request.originalUrl, request.params);
+    console.log(theKey);
+    theKeys.push(theKey);
+
+    client.exists(theKey, (_,itExists) => {
+        if (itExists) {
+            client.get(theKey, (_, theResponse) => {
+                response.json(JSON.parse(theResponse));    
+            });
+        }
+        else {
+            // on copie la méthode utilisée pour envoyer la réponse
+            // ouais, on est comme ça
+
+            // plus sérieusement, le fait de copier une fonction entraîne une perte du contexte
+            // (si la fonction fait appel à this, il ne désignera plus rien)
+            // c'est pour ça qu'on appelle bind derrière, qui permet de redéfinir le this de la fonction
+            const sendJson = response.json.bind(response);
+            response.json = (theResponse) => {
+                client.setex(
+                    theKey,
+                    process.env.REDIS_EXPIRY || 5,
+                    JSON.stringify(theResponse),
+                    _ => sendJson(theResponse)
+                );
+            }
+            next();
+        }
+
+    });
+};
+
+const flush = (request,response,next) => {
+
+    client.del(theKeys, _ => {
+        theKeys.length = 0;
+        next();
+    });
+}
+
+module.exports = {cache, flush};
--- BACK/app/router.js
+++ BACK/app/router.js
@@ -2,13 +2,15 @@ const { Router } = require('express');
 const router = Router();
 const mainController = require('./controllers/mainController');
 const {validateQuery,validateBody} = require('./services/validator');
+const {cache, flush} = require('./cache/cacheStrategy');
 const {insertUserSchema,updateUserSchema} = require('./schemas/userschema');
 const User = require('./models/User');
 
-router.get('/users', mainController.findAll(User));
-router.get('/users/user/:id', mainController.findOne(User));
-router.post('/users', validateBody(insertUserSchema), mainController.insertOne(User));
-router.patch('/users/user/:id', validateBody(updateUserSchema), mainController.updateOne(User));
-router.delete('/users/user/:id', mainController.deleteOne(User));
+router.get('/users',cache , mainController.findAll(User));
+router.get('/users/user/:id',cache , mainController.findOne(User));
+router.post('/users', validateBody(insertUserSchema), flush, mainController.insertOne(User));
+router.patch('/users/user/:id', validateBody(updateUserSchema),flush ,mainController.updateOne(User));
+router.delete('/users/user/:id',flush, mainController.deleteOne(User));
+
 
 module.exports = router;

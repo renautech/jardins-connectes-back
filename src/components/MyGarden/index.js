9fc25b694cfbf5a12fc5c1685404f91f41472b2b
Fle0h <davidf69220@gmail.com>
2020-10-21 20:46:48 +0200

    minor errors correct

--- src/components/MyGarden/index.js
+++ src/components/MyGarden/index.js
@@ -8,7 +8,7 @@ import './style.scss';
 
 const MyGarden = ({ dataBoard }) => {
   const Families = dataBoard.map((family) => {
-    return <Family type={family.name} img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcThP4gFDgQdTU9TPf_yTOEcyPFC31nkX5qQlg&usqp=CAU" />
+    return <Family type={family.name} img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcThP4gFDgQdTU9TPf_yTOEcyPFC31nkX5qQlg&usqp=CAU" />;
   });
 
   return (

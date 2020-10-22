214472a85c718da784d83593584fc46168c74d29
Tidus91 <incorvaia.tony@gmail.com>
2020-10-22 14:47:13 +0200

    middleware Authentificate session/role to verify

---
+++ BACK/app/services/session.js
@@ -0,0 +1,27 @@
+const isAuthentificate = async (req,res,next) => {
+    if (req.session.user) {
+        next();
+    } else {
+        res.json("Vous n'êtes pas connecté");
+    }
+}
+
+const isAdmin = async (req,res,next) => {
+    if (req.session.user.role === 'admin') {
+        next();
+    }
+    else {
+        res.json("Vous n'avez pas l'accès (admin requis)");
+    }
+}
+
+const isJardinier = async (req,res,next) => {
+    if(req.session.user.role === ('jardinier' || 'admin')) {
+        next();
+    }
+    else {
+        res.json("Vous n'avez pas accès à cette page");
+    }
+}
+
+module.exports = {isAuthentificate,isAdmin,isJardinier};

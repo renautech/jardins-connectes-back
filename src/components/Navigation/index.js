051ae87c898b248c1fc73a44fd69ecf3a256e559
Fle0h <davidf69220@gmail.com>
2020-10-23 09:47:53 +0200

    minor correct

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,16 +4,19 @@ import { NavLink } from 'react-router-dom';
 
 import './style.scss';
 
-const Navigation = ({isLogged}) => {
+const Navigation = ({ isLogged }) => {
+  console.log(isLogged);
   return (
     <div className="navigation">
       <nav className="navigation__nav">
         <NavLink to="/">
           <p className="navigation__nav__link">Accueil</p>
         </NavLink>
-        { isLogged && <NavLink to="/mon-jardin">
+        {isLogged && (
+        <NavLink to="/mon-jardin">
           <p className="navigation__nav__link">Mon Jardin</p>
-        </NavLink> }
+        </NavLink>
+        )}
       </nav>
     </div>
   );

8fda73c871dd70f3275e5f4175303ea1fb255fed
Flyworkz <pedron.dorian@gmail.com>
2020-10-26 12:14:31 +0100

    fix error familyController

--- BACK/app/controllers/familyController.js
+++ BACK/app/controllers/familyController.js
@@ -3,7 +3,11 @@ const Family = require('../models/Family');
 const familyController = {
 
     findWhereVoidBoardForConnectedUser: async (req, res) => {
-        res.json(await Family.findWhereVoidBoardByUser(req.session.user.id));
+        if (req.session.user) {
+            res.json(await Family.findWhereVoidBoardByUser(req.session.user.id));
+        } else {
+            res.json('Veuillez vous connecter avant d\'accéder à votre jardin');
+        }
     }
     
 }

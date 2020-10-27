5ee715b2ce778e1e5c7011090735460f7a07c59d
Flyworkz <pedron.dorian@gmail.com>
2020-10-27 14:30:28 +0100

    update cors

--- BACK/app/server.js
+++ BACK/app/server.js
@@ -13,7 +13,8 @@ const host = process.env.HOST || "localhost";
 
 // Accès à l'API
 app.use(cors({
-	origin: '*'
+    credentials: true,
+	origin: 'http://localhost:8080'
 }));
 
 app.use(express.json());

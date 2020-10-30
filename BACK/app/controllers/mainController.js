066ffe676ca2d448bed2122ab0bb472c399e036e
Flyworkz <pedron.dorian@gmail.com>
2020-10-30 16:40:39 +0100

    notFound + try/catch maincontroller

--- BACK/app/controllers/mainController.js
+++ BACK/app/controllers/mainController.js
@@ -3,43 +3,84 @@
 const mainController = {
 
     findAll: (model) => async (_,res) => {
-    
-        res.json(await model.findAll());
+        try {
+            res.json(await model.findAll());
+        } catch (err) {
+            res.json({
+                message: err.message,
+                state: false
+            });
+        }
     },
 
     findOne: (model) => async (req,res) => {
-
-        res.json(await model.findOne(req.params.id));
+        try {
+            res.json(await model.findOne(req.params.id));
+        } catch (err) {
+            res.json({
+                message: err.message,
+                state: false
+            });
+        } 
     },
 
     insertOne: (model) => async (req,res) => {
-
-        const newEntrance = new model(req.body);
-        await newEntrance.save();
-        res.json(newEntrance);
+        try {
+            const newEntrance = new model(req.body);
+            await newEntrance.save();
+            res.json(newEntrance);
+        } catch (err) {
+            res.json({
+                message: err.message,
+                state: false
+            });
+        }
     },
 
     updateOne: (model) => async (req,res) => {
-
-        const toUpdate = await model.findOne(req.params.id);
-        const modelUpdate = new model(toUpdate);
-        for(const prop in req.body) {
-            modelUpdate[prop] = req.body[prop];
+        try {
+            const toUpdate = await model.findOne(req.params.id);
+            const modelUpdate = new model(toUpdate);
+            for(const prop in req.body) {
+                modelUpdate[prop] = req.body[prop];
+            }
+            await modelUpdate.save();
+            res.json(modelUpdate);
+        } catch (err) {
+            res.json({
+                message: err.message,
+                state: false
+            });
         }
-        await modelUpdate.save();
-        res.json(modelUpdate);
+        
     },
 
     deleteOne: (model) => async (req,res) => {
-        const toDelete = await model.findOne(req.params.id);
-        const modelDelete = new model(toDelete);
-        await modelDelete.delete();
-        if (modelDelete.errorMessage) {
-            res.status(400).json(modelDelete.errorMessage);
-        } else {
-            res.json(`L'Utilisateur ayant l'ID : ${req.params.id} a été supprimé`);
-        }
-    }
+        try {
+            const toDelete = await model.findOne(req.params.id);
+            const modelDelete = new model(toDelete);
+            await modelDelete.delete();
+            if (modelDelete.errorMessage) {
+                throw new Error(modelDelete.errorMessage);
+            }
+            res.json({
+                message: `L'Utilisateur ayant l'ID : ${req.params.id} a été supprimé`,
+                state: true
+            });
+        } catch (err) {
+            res.json({
+                message: err.message,
+                state: false
+            });
+        }       
+    },
+
+    notFound: (req, res) => {
+        res.status(404).json({
+            message: "Cette route n'existe pas !",
+            state: false
+        });
+    },
 
 }
 
